c5dc2464d16698a4fa78fcd8bed789a1fbddfbc7
DimasJolan <71343073+DimasJolan@users.noreply.github.com>
2020-10-01 14:38:41 +0300

    Add files via upload

--- myjavascript.js
+++ myjavascript.js
@@ -1,10 +1,10 @@
 "use strict";
 let prev = 1;
 function calculate(x) {
-  return x/prev;
+  return Math.round(x / prev);
   };
 
-console.log(prev = calculate(2));
+console.log(prev = calculate(3));
 console.log(prev = calculate(10));
 console.log(prev = calculate(20));
 
